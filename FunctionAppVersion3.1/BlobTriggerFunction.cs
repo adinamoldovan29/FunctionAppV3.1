b148c294c15e207be3e6c1760150dff338690463
Adina Moldovan <adina.moldovan@vizolution.co.uk>
2020-03-04 10:50:26 +0200

    Add conection string to blob storage func

--- FunctionAppVersion3.1/BlobTriggerFunction.cs
+++ FunctionAppVersion3.1/BlobTriggerFunction.cs
@@ -9,7 +9,8 @@ namespace FunctionAppVersion3._1
     public static class BlobTriggerFunction
     {
         [FunctionName("BlobTriggerFunction")]
-        public static void Run([BlobTrigger("container04032020/{name}", Connection = "")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("container04032020/{name}", 
+            Connection = "DefaultEndpointsProtocol=https;AccountName=storeageaccount04032020;AccountKey=SrJxUF0AQWOP9ICBxQtanr68mn/CRnbu23oKQE1kRILqu/W6FbqZCr7veZGXFlWYRXScyGj7QTMebCgYXcd2dw==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
